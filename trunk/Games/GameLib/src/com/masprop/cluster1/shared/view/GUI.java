927f939ba9d37830477015049d42767ee50cc90e
michal.babacek <michal.babacek@63bd3704-b108-11de-8db9-a33e7b47b29e>
2009-11-06 01:21:07 +0000

    Generated JavaDoc in GameLib project.

--- trunk/Games/GameLib/src/com/masprop/cluster1/shared/view/GUI.java
+++ trunk/Games/GameLib/src/com/masprop/cluster1/shared/view/GUI.java
@@ -1,13 +1,14 @@
-/*
- * GUI.java
- *
- * Created on 06-Nov-2009, 01:45:48
- */
 package com.masprop.cluster1.shared.view;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 
 /**
  *
- * @author Karm
+ * @author Michal Karm Babacek
  */
 public class GUI extends javax.swing.JFrame {
 
@@ -25,33 +26,59 @@ public class GUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jSplitPane1 = new javax.swing.JSplitPane();
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
         menuBar = new javax.swing.JMenuBar();
         fileMenu = new javax.swing.JMenu();
-        openMenuItem = new javax.swing.JMenuItem();
-        saveMenuItem = new javax.swing.JMenuItem();
-        saveAsMenuItem = new javax.swing.JMenuItem();
+        newGameMenuItem = new javax.swing.JMenuItem();
+        loadGameMenuItem = new javax.swing.JMenuItem();
+        scoresMenuItem = new javax.swing.JMenuItem();
         exitMenuItem = new javax.swing.JMenuItem();
-        editMenu = new javax.swing.JMenu();
-        cutMenuItem = new javax.swing.JMenuItem();
-        copyMenuItem = new javax.swing.JMenuItem();
-        pasteMenuItem = new javax.swing.JMenuItem();
-        deleteMenuItem = new javax.swing.JMenuItem();
         helpMenu = new javax.swing.JMenu();
-        contentsMenuItem = new javax.swing.JMenuItem();
+        manualMenuItem = new javax.swing.JMenuItem();
         aboutMenuItem = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jPanel1.setBackground(new java.awt.Color(187, 178, 178));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 93, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 279, Short.MAX_VALUE)
+        );
+
+        jSplitPane1.setLeftComponent(jPanel1);
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 302, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 279, Short.MAX_VALUE)
+        );
+
+        jSplitPane1.setRightComponent(jPanel2);
+
         fileMenu.setText("File");
 
-        openMenuItem.setText("Open");
-        fileMenu.add(openMenuItem);
+        newGameMenuItem.setText("New game");
+        fileMenu.add(newGameMenuItem);
 
-        saveMenuItem.setText("Save");
-        fileMenu.add(saveMenuItem);
+        loadGameMenuItem.setText("Load game");
+        fileMenu.add(loadGameMenuItem);
 
-        saveAsMenuItem.setText("Save As ...");
-        fileMenu.add(saveAsMenuItem);
+        scoresMenuItem.setText("Scores");
+        fileMenu.add(scoresMenuItem);
 
         exitMenuItem.setText("Exit");
         exitMenuItem.addActionListener(new java.awt.event.ActionListener() {
@@ -63,26 +90,10 @@ public class GUI extends javax.swing.JFrame {
 
         menuBar.add(fileMenu);
 
-        editMenu.setText("Edit");
-
-        cutMenuItem.setText("Cut");
-        editMenu.add(cutMenuItem);
-
-        copyMenuItem.setText("Copy");
-        editMenu.add(copyMenuItem);
-
-        pasteMenuItem.setText("Paste");
-        editMenu.add(pasteMenuItem);
-
-        deleteMenuItem.setText("Delete");
-        editMenu.add(deleteMenuItem);
-
-        menuBar.add(editMenu);
-
         helpMenu.setText("Help");
 
-        contentsMenuItem.setText("Contents");
-        helpMenu.add(contentsMenuItem);
+        manualMenuItem.setText("Manual");
+        helpMenu.add(manualMenuItem);
 
         aboutMenuItem.setText("About");
         helpMenu.add(aboutMenuItem);
@@ -95,11 +106,11 @@ public class GUI extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jSplitPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 279, Short.MAX_VALUE)
+            .addComponent(jSplitPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 279, Short.MAX_VALUE)
         );
 
         pack();
@@ -110,6 +121,7 @@ public class GUI extends javax.swing.JFrame {
     }//GEN-LAST:event_exitMenuItemActionPerformed
 
     /**
+     * Run the main thread.
      * @param args the command line arguments
      */
     public static void main(String args[]) {
@@ -120,20 +132,212 @@ public class GUI extends javax.swing.JFrame {
             }
         });
     }
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenuItem aboutMenuItem;
-    private javax.swing.JMenuItem contentsMenuItem;
-    private javax.swing.JMenuItem copyMenuItem;
-    private javax.swing.JMenuItem cutMenuItem;
-    private javax.swing.JMenuItem deleteMenuItem;
-    private javax.swing.JMenu editMenu;
     private javax.swing.JMenuItem exitMenuItem;
     private javax.swing.JMenu fileMenu;
     private javax.swing.JMenu helpMenu;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JSplitPane jSplitPane1;
+    private javax.swing.JMenuItem loadGameMenuItem;
+    private javax.swing.JMenuItem manualMenuItem;
     private javax.swing.JMenuBar menuBar;
-    private javax.swing.JMenuItem openMenuItem;
-    private javax.swing.JMenuItem pasteMenuItem;
-    private javax.swing.JMenuItem saveAsMenuItem;
-    private javax.swing.JMenuItem saveMenuItem;
+    private javax.swing.JMenuItem newGameMenuItem;
+    private javax.swing.JMenuItem scoresMenuItem;
     // End of variables declaration//GEN-END:variables
+
+    /*Getters and Setters*/
+    /**
+     *
+     * @return
+     */
+    public JMenuItem getAboutMenuItem() {
+        return aboutMenuItem;
+    }
+
+    /**
+     *
+     * @param aboutMenuItem
+     */
+    public void setAboutMenuItem(JMenuItem aboutMenuItem) {
+        this.aboutMenuItem = aboutMenuItem;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public JMenuItem getExitMenuItem() {
+        return exitMenuItem;
+    }
+
+    /**
+     *
+     * @param exitMenuItem
+     */
+    public void setExitMenuItem(JMenuItem exitMenuItem) {
+        this.exitMenuItem = exitMenuItem;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public JMenu getFileMenu() {
+        return fileMenu;
+    }
+
+    /**
+     *
+     * @param fileMenu
+     */
+    public void setFileMenu(JMenu fileMenu) {
+        this.fileMenu = fileMenu;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public JMenu getHelpMenu() {
+        return helpMenu;
+    }
+
+    /**
+     *
+     * @param helpMenu
+     */
+    public void setHelpMenu(JMenu helpMenu) {
+        this.helpMenu = helpMenu;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public JPanel getjPanel1() {
+        return jPanel1;
+    }
+
+    /**
+     *
+     * @param jPanel1
+     */
+    public void setjPanel1(JPanel jPanel1) {
+        this.jPanel1 = jPanel1;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public JPanel getjPanel2() {
+        return jPanel2;
+    }
+
+    /**
+     *
+     * @param jPanel2
+     */
+    public void setjPanel2(JPanel jPanel2) {
+        this.jPanel2 = jPanel2;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public JSplitPane getjSplitPane1() {
+        return jSplitPane1;
+    }
+
+    /**
+     *
+     * @param jSplitPane1
+     */
+    public void setjSplitPane1(JSplitPane jSplitPane1) {
+        this.jSplitPane1 = jSplitPane1;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public JMenuItem getLoadGameMenuItem() {
+        return loadGameMenuItem;
+    }
+
+    /**
+     *
+     * @param loadGameMenuItem
+     */
+    public void setLoadGameMenuItem(JMenuItem loadGameMenuItem) {
+        this.loadGameMenuItem = loadGameMenuItem;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public JMenuItem getManualMenuItem() {
+        return manualMenuItem;
+    }
+
+    /**
+     *
+     * @param manualMenuItem
+     */
+    public void setManualMenuItem(JMenuItem manualMenuItem) {
+        this.manualMenuItem = manualMenuItem;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public JMenuBar getMainMenuBar() {
+        return menuBar;
+    }
+
+    /**
+     *
+     * @param menuBar
+     */
+    public void setMainMenuBar(JMenuBar menuBar) {
+        this.menuBar = menuBar;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public JMenuItem getNewGameMenuItem() {
+        return newGameMenuItem;
+    }
+
+    /**
+     *
+     * @param newGameMenuItem
+     */
+    public void setNewGameMenuItem(JMenuItem newGameMenuItem) {
+        this.newGameMenuItem = newGameMenuItem;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public JMenuItem getScoresMenuItem() {
+        return scoresMenuItem;
+    }
+
+    /**
+     *
+     * @param scoresMenuItem
+     */
+    public void setScoresMenuItem(JMenuItem scoresMenuItem) {
+        this.scoresMenuItem = scoresMenuItem;
+    }
 }
