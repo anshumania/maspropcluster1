e1ad73b2c6630f5d29b2d424e860c93c98682bbc
pleva.matus <pleva.matus@63bd3704-b108-11de-8db9-a33e7b47b29e>
2009-11-14 20:21:18 +0000

    GameGenerator correction

--- Games/GameLib/src/com/masprop/cluster1/shared/controller/GameGenerator.java
+++ Games/GameLib/src/com/masprop/cluster1/shared/controller/GameGenerator.java
@@ -1,7 +1,7 @@
 package com.masprop.cluster1.shared.controller;
 
+import com.masprop.cluster1.shared.model.Constraint;
 import com.masprop.cluster1.shared.model.Game;
-
 
 // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
 // #[regen=yes,id=DCE.E7F8DC8B-37D6-1BDD-2282-0DFB7A837DB9]
@@ -14,19 +14,18 @@ import com.masprop.cluster1.shared.model.Game;
  * @see GameManager
  * 
  * @version 1.0
- * @since 1.0	
+ * @since 1.0
  */
 public interface GameGenerator {
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.21E81485-6016-A042-DF52-89668D5A714C]
     // </editor-fold> 
-	/**
-	 *	This method generates and return a complete Game to the caller.
-	 *
-	 *  @return Game
-	 */
-    public Game generateGame ();
-
+    /**
+     *	This method generates and return a complete Game to the caller.
+     *
+     *  @return Game
+     */
+    public Game generateGame(Constraint constraint);
 }
 
