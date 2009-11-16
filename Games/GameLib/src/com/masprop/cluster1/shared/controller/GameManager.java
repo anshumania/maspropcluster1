65d6f668314cd0eee96a1cc0e01e57ce6c5ccacb
mehta.anshuman <mehta.anshuman@gmail.com>
2009-11-16 01:39:59 +0000

    added logic for loading the respective game controllers. may have to change the method names to make them more intuitive to their function

--- Games/GameLib/src/com/masprop/cluster1/shared/controller/GameManager.java
+++ Games/GameLib/src/com/masprop/cluster1/shared/controller/GameManager.java
@@ -5,10 +5,6 @@ import java.util.List;
 import com.masprop.cluster1.shared.model.Constraint;
 import com.masprop.cluster1.shared.model.Game;
 
-// TODO: Auto-generated Javadoc
-// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-// #[regen=yes,id=DCE.688451B6-74F9-0A7F-1CE3-18B04D11802E]
-// </editor-fold> 
 /**
  * This Interface provides the management operations on a <tt>Game</tt>
  * 
@@ -35,9 +31,6 @@ import com.masprop.cluster1.shared.model.Game;
  */
 public interface GameManager {
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
-    // #[regen=yes,id=DCE.61AA2A5F-7539-5F4E-0562-3E4A1356EC6A]
-    // </editor-fold> 
     /**
      * Gets the game generator.
      *
@@ -45,9 +38,6 @@ public interface GameManager {
      */
     public GameGenerator getGameGenerator();
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.EFE06DD9-22B7-08F4-A074-278B691222C3]
-    // </editor-fold> 
     /**
      * Gets the game solver.
      * 
@@ -55,9 +45,7 @@ public interface GameManager {
      */
     public GameSolver getGameSolver();
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.49E36902-A6FD-F6E4-D8ED-83E0F083DB57]
-    // </editor-fold> 
+     
     /**
      * Gets the game validator.
      * 
@@ -72,9 +60,7 @@ public interface GameManager {
      */
     public StorageManager getStorageManager();
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.332EE15D-E0F3-E8CB-C86F-E9AC53717622]
-    // </editor-fold> 
+     
     /**
      * Gets the new game.
      * 
@@ -84,9 +70,7 @@ public interface GameManager {
      */
     public Game getNewGame(Constraint constraint);
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.28E1D2EC-9E15-5BD4-59BE-78D8E79D8A6B]
-    // </editor-fold> 
+     
     /**
      * Validate game.
      * 
@@ -96,9 +80,7 @@ public interface GameManager {
      */
     public boolean validateGame(Game game);
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.223FCFE8-721B-5A88-5B05-E43BDB0EE4DA]
-    // </editor-fold> 
+     
     /**
      * Solve game.
      * 
@@ -108,9 +90,7 @@ public interface GameManager {
      */
     public Game solveGame(Game game);
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.67372566-AF1D-86C6-37A8-B83133198391]
-    // </editor-fold> 
+     
     /**
      * Save game.
      * 
@@ -118,9 +98,7 @@ public interface GameManager {
      */
     public void saveGame(Game game);
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.CC2D028C-69E2-ACA8-A490-CE51B71F4BFE]
-    // </editor-fold> 
+     
     /**
      * Load game.
      * 
@@ -128,9 +106,7 @@ public interface GameManager {
      */
     public List<Game> loadGame();
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.033EE7D3-E0B9-968F-4D33-D5FD336430FB]
-    // </editor-fold> 
+     
     /**
      * Gets the statistics.
      * 
