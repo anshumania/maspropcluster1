478400f79ebd17bdc61d3875d892d15a164a99fd
mehta.anshuman <mehta.anshuman@gmail.com>
2009-11-16 21:42:11 +0000

    Added Sudoku implementaion of shared classes + test cases

---
+++ Games/Sudoku/src/com/masprop/cluster1/sudoku/controller/SudokuGameValidator.java
@@ -0,0 +1,14 @@
+package com.masprop.cluster1.sudoku.controller;
+
+import com.masprop.cluster1.shared.controller.GameValidator;
+import com.masprop.cluster1.shared.model.Game;
+
+public class SudokuGameValidator implements GameValidator {
+
+	@Override
+	public boolean validateGame(Game game) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ Games/Sudoku/src/com/masprop/cluster1/sudoku/controller/SudokuStorageManager.java
@@ -0,0 +1,7 @@
+package com.masprop.cluster1.sudoku.controller;
+
+import com.masprop.cluster1.shared.controller.StorageManager;
+
+public class SudokuStorageManager extends StorageManager {
+
+}
