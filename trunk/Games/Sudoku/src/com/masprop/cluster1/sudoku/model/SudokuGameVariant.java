7b253b81a7d3a05213cffcf5f5d4d72a27c66c65
mehta.anshuman@gmail.com <mehta.anshuman@gmail.com@63bd3704-b108-11de-8db9-a33e7b47b29e>
2009-11-16 23:29:25 +0000

    GameVariant for Sudoku added ( required for the updated constructor of Constraint)

---
+++ trunk/Games/Sudoku/src/com/masprop/cluster1/sudoku/model/SudokuGameVariant.java
@@ -0,0 +1,11 @@
+package com.masprop.cluster1.sudoku.model;
+
+import com.masprop.cluster1.shared.model.GameVariant;
+
+public enum SudokuGameVariant implements GameVariant {
+	
+	NINEcrossNINE,
+	
+	FIVEcrossFIVE
+
+}
