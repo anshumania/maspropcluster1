7d2694cd5d56030dde646840deace4241d40e316
pleva.matus <pleva.matus@63bd3704-b108-11de-8db9-a33e7b47b29e>
2009-11-17 20:13:37 +0000

    Direction, HidatoStatisticManager

---
+++ Games/Hidato/HidatoApplication/src/biz/karms/hidato/app/game/matrix/impl/Direction.java
@@ -0,0 +1,17 @@
+package biz.karms.hidato.app.game.matrix.impl;
+
+/**
+ * Enumerate possible movements from current cell.
+ *
+ * @author Matus Pleva
+ */
+public enum Direction {
+    LEFTTOP,
+    TOP,
+    RIGHTTOP,
+    RIGHT,
+    RIGHTBOTTOM,
+    BOTTOM,
+    LEFTBOTTOM,
+    LEFT;
+}
