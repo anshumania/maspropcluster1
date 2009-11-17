ef3a543ed2388363ec6113cf8b7b9b067b937b71
pleva.matus <pleva.matus@63bd3704-b108-11de-8db9-a33e7b47b29e>
2009-11-17 20:13:37 +0000

    Direction, HidatoStatisticManager

---
+++ trunk/Games/Hidato/HidatoApplication/src/biz/karms/hidato/app/controller/impl/HidatoStatisticsManager.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package biz.karms.hidato.app.controller.impl;
+
+import com.masprop.cluster1.shared.controller.StatisticsManager;
+import com.masprop.cluster1.shared.model.Game;
+import java.util.List;
+
+/**
+ *
+ * @author matusko
+ */
+class HidatoStatisticsManager implements StatisticsManager {
+
+    public HidatoStatisticsManager() {
+    }
+
+    public void getScoreFor(Game game) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setScoreFor(Game game) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public List<Game> loadGame() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void saveGame(Game game) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public List<String> getStatistics() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
