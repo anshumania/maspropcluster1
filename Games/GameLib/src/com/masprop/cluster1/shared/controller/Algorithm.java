801096d7ff81ac9fa53264f26e4357ce797a57ab
nick.veenhof <nick.veenhof@63bd3704-b108-11de-8db9-a33e7b47b29e>
2009-11-18 00:00:24 +0000

    fixed typo

--- Games/GameLib/src/com/masprop/cluster1/shared/controller/Algorithm.java
+++ Games/GameLib/src/com/masprop/cluster1/shared/controller/Algorithm.java
@@ -3,14 +3,14 @@ package com.masprop.cluster1.shared.controller;
 import com.masprop.cluster1.shared.model.Game;
 
 // TODO: Auto-generated Javadoc
-// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+// <editor-fold defaultstate="collapsed" desc=" UML Marker ">
 // #[regen=yes,id=DCE.7C93EA8C-17C2-FD5F-84F4-9410B867A429]
-// </editor-fold> 
+// </editor-fold>
 /**
  * The interface to implement if there are specific algorithms
  * that have been implemented for <tt>Game</tt> operations.
  * Should be injected into the Controllers.
- * 
+ *
  * @see Game
  * @see GameGenerator
  * @see GameSolver
@@ -21,14 +21,14 @@ import com.masprop.cluster1.shared.model.Game;
  */
 public interface Algorithm {
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
     // #[regen=yes,id=DCE.84180A70-9CFF-D143-0013-17AC1E83CA83]
-    // </editor-fold> 
+    // </editor-fold>
     /**
      * Perform the operations defined by the algorithm on the Game.
-     * 
+     *
      * @param game the game
      */
-    public void performArlogrithmics(Game game);
+    public void performAlgorithmics(Game game);
 }
 
