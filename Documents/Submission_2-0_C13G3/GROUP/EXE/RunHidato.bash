a483913a749da22f452636c945173e0112f4ac01
michal.babacek <michal.babacek@63bd3704-b108-11de-8db9-a33e7b47b29e>
2009-11-20 18:02:33 +0000

    Delivery 2, Hidato test runnable as TestHidato.bash

--- Documents/Submission_2-0_C13G3/GROUP/EXE/RunHidato.bash
+++ Documents/Submission_2-0_C13G3/GROUP/EXE/RunHidato.bash
@@ -1,3 +1,8 @@
 #!/bin/bash
+#
+# Author: Michal "Karm" Babacek
+#
+# Script version: 1
+#
 
 java -cp "Hidato/lib/GameLib.jar:Hidato/HidatoApplication.jar" biz.karms.hidato.app.controller.impl.HidatoApplicationController
