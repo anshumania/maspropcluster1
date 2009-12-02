2f152095fd5b523d93b3ab153e76f910f2605fb9
daunou.adrien <daunou.adrien@free.fr>
2009-12-02 23:53:31 +0000

    StorageManager updated

--- Games/GameLib/src/com/masprop/cluster1/shared/controller/StorageManager.java
+++ Games/GameLib/src/com/masprop/cluster1/shared/controller/StorageManager.java
@@ -3,18 +3,19 @@ package com.masprop.cluster1.shared.controller;
 
 import com.masprop.cluster1.shared.model.Game;
 import com.masprop.cluster1.shared.model.GameType;
-import com.thoughtworks.xstream.XStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import com.thoughtworks.xstream.io.xml.DomDriver;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+import com.thoughtworks.xstream.XStream;
 
-// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+// <editor-fold defaultstate="collapsed" desc=" UML Marker ">
 // #[regen=yes,id=DCE.878F01D5-7373-FE1B-BF53-79E3DCE4A0A7]
-// </editor-fold> 
+// </editor-fold>
 /**
  * This class handles storage of Game objects to XML files
  *
@@ -22,28 +23,27 @@ import java.util.logging.Logger;
  */
 public class StorageManager {
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
     // #[regen=yes,id=DCE.A2118DC1-83B3-370A-CEA1-FD9A7FCCB486]
-    // </editor-fold> 
+    // </editor-fold>
     /**
      * Class constructor.
      */
     public StorageManager() {
     }
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
     // #[regen=yes,id=DCE.49ACEC12-CEF8-0A0E-0C32-26CCB71B08C1]
     // </editor-fold>
     /**
      * Save game to a XML file.
      * Serialize the object using XStream library
-     * 
+     *
      * @param game the game
      * @param file Filepath
      * @param gameType Type of Game
      */
-    public void saveToFile(Game game, File file, GameType gameType) {
-        // Game gameTest = ApplicationController.getUniqueInstance().getGameManager().getNewGame(null);
+    public void saveToFile(Game game, File file, GameType gameType) throws IOException {
         XStream xs = new XStream();
         try {
             FileOutputStream fileoutputstream = new FileOutputStream(file);
@@ -52,8 +52,9 @@ public class StorageManager {
             e.printStackTrace();
         }
     }
-
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+   
+    
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
     // #[regen=yes,id=DCE.C2D22A47-457D-70F9-F7A6-49A2F1FAB7C9]
     // </editor-fold>
     /**
@@ -64,22 +65,21 @@ public class StorageManager {
      * @param file Filepath
      * @param gameType Type of Game
      */
-     public Game loadFromFile(File file, GameType gameType) {
+     public Game loadFromFile(File file, GameType gameType) throws IOException {
         XStream xs = new XStream(new DomDriver());
-         Class game = null; //= new Game();
+        Class game = null;
          try {
              if (gameType == GameType.SUDOKU) {
                  game = Class.forName("com.masprop.cluster1.sudoku.model.SudokuGame");
                  FileInputStream fileinputstream = new FileInputStream(file);
-                 Game y = (Game)game.cast(xs.fromXML(fileinputstream, game));
-                 return y;
+                 Game sudokugame = (Game)game.cast(xs.fromXML(fileinputstream, game));
+                 return sudokugame;
              }
              if (gameType == GameType.HIDATO) {
                  game = Class.forName("biz.karms.hidato.app.game.impl.HidatoGame");
                  FileInputStream fileinputstream = new FileInputStream(file);
-                 Object y = (xs.fromXML(fileinputstream, game));
-                 Game z = (Game)game.cast(xs.fromXML(fileinputstream, game));
-                 return z;
+                 Game hidatogame = (Game) xs.fromXML(fileinputstream);
+               return hidatogame;
              }
          } catch (ClassNotFoundException ex) {
              Logger.getLogger(StorageManager.class.getName()).log(Level.SEVERE, null, ex);
@@ -87,8 +87,6 @@ public class StorageManager {
              e.printStackTrace();
          }
        return null;
-
-    }
-
+     }
 }
 
