ed49cf9a9580f55131be96e3cb92eb209b272e9b
pleva.matus <pleva.matus@63bd3704-b108-11de-8db9-a33e7b47b29e>
2009-12-03 17:16:55 +0000

    GameLib - added Coordinates, added Direction, modified Matrix (2D array + methods from our HidatoApplication)

---
+++ trunk/Games/GameLib/src/com/masprop/cluster1/shared/model/Direction.java
@@ -0,0 +1,17 @@
+package com.masprop.cluster1.shared.model;
+
+/**
+ * Enumerate possible movements from current cell.
+ *
+ * @author Matus Pleva
+ */
+public enum Direction {
+    LEFTTOP,
+    TOP,
+    RIGHTTOP,
+    RIGHT,
+    RIGHTBOTTOM,
+    BOTTOM,
+    LEFTBOTTOM,
+    LEFT;
+}
