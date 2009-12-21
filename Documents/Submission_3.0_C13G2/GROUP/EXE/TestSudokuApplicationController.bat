4cf2ccda0a8461601ec2de3a22c7e92c95a4da87
mehta.anshuman <mehta.anshuman@gmail.com>
2009-12-21 01:19:13 +0000

    - added tests documents for final submission

---
+++ Documents/Submission_3.0_C13G2/GROUP/EXE/TestSudokuApplicationController.bat
@@ -0,0 +1,5 @@
+@ECHO off
+DIR  . 
+java -cp ../SOURCE/junit-4.5.jar;../SOURCE/sudoku.jar org.junit.runner.JUnitCore com.masprop.cluster1.sudoku.controller.SudokuApplicationControllerTest
+ECHO "The above exception is OKay ! It is because the GUI was terminated by the test case"
+PAUSE
